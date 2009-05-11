8d802bcedab8891c40cae4780a728a4f352bdb07
Jan Berkel <jan.berkel@gmail.com>
2009-05-11 21:04:44 +0100

    use an anonymous function

--- js/app.js
+++ js/app.js
@@ -9,33 +9,31 @@ $(function() {
   });
 });
 
-
-function pipeCallback(payload) {
-  var items = payload['value']['items'];
-  for (var i=0; i<items.length; i++) {
-    var rating = "";        
-    var rCount = parseInt(items[i]['user_rating'], 10);
-    if (rCount != NaN) {
-      rating += " (";
-      for (var r=0; r<rCount; r++) {
-        rating += "&#9733;";                
-      }
-      rating += ")";
-    }
-      
-    $("#currently-reading").append("<a href='" + items[i]['book_large_image_url']  + "'><img src='" + items[i]['book_small_image_url'] + "' class='left-img' /></a>")        
-    $("#currently-reading").append("<p>&laquo;" + "<a href='" + items[i]['link'] + "'>" + items[i]['y:title'] + "</a>"+ "&raquo;" +
-      " by " + items[i]['author_name']  + rating + "</p>");
-      
-    $("#currently-reading").append("<blockquote>&ldquo;" + items[i]['user_review'] + "&rdquo;</blockquote>");
-    
-    $("#currently-reading").append("</div>")            
-    $("#currently-reading").append("<div class='clear'/>")         
-  }
-};
-
-
 (function($) {
-  $.getScript("http://pipes.yahoo.com/pipes/pipe.run?_id=xL8fCU4_3hGJGTah3nBDOQ&_render=json&_callback=pipeCallback");    
+  $.getJSON("http://pipes.yahoo.com/pipes/pipe.run?_id=xL8fCU4_3hGJGTah3nBDOQ&_render=json&_callback=?",
+    function (payload) {
+      var items = payload['value']['items'];
+      for (var i=0; i<items.length; i++) {
+        var rating = "";        
+        var rCount = parseInt(items[i]['user_rating'], 10);
+        if (rCount != NaN) {
+          rating += " (";
+          for (var r=0; r<rCount; r++) {
+            rating += "&#9733;";                
+          }
+          rating += ")";
+        }
+
+        $("#currently-reading").append("<a href='" + items[i]['book_large_image_url']  + "'><img src='" + items[i]['book_small_image_url'] + "' class='left-img' /></a>")        
+        $("#currently-reading").append("<p>&laquo;" + "<a href='" + items[i]['link'] + "'>" + items[i]['y:title'] + "</a>"+ "&raquo;" +
+          " by " + items[i]['author_name']  + rating + "</p>");
+
+        $("#currently-reading").append("<blockquote>&ldquo;" + items[i]['user_review'] + "&rdquo;</blockquote>");
+
+        $("#currently-reading").append("</div>")            
+        $("#currently-reading").append("<div class='clear'/>")         
+      }
+    });
+    
 })(jQuery);
       
